d17de7519d8fbfb189db9c9b0a660b9e30ba7f6f
SIDDHARTH SAHOO <sahoosiddharth3@gmail.com>
2021-02-05 02:42:23 +0530

    Adding images using input field

--- likely-to-predict/src/App.js
+++ likely-to-predict/src/App.js
@@ -27,9 +27,8 @@ let App = (props) => {
   },[isShowing])
 
   return (
-    <section className="flex justify-center">
-      {console.log("re-rendered")}
-      <div className="w-1/2 text-center">
+    <section className="flex justify-center w-full ">
+      <div className="w-10/15 text-center">
         <div className="my-4 text-center">
           {props.title} {title}
         </div>
@@ -49,38 +48,4 @@ let App = (props) => {
   );
 };
 
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { title: "Hello React", isShowing: false };
-//   }
-
-//   handleClick = () => {
-//     this.setState({ isShowing: !this.state.isShowing });
-//   };
-
-//   render() {
-//     return (
-// <section className="flex justify-center">
-//   <div className="w-1/2 text-center">
-//     <div className="my-4 text-center">{this.state.title}</div>
-//     <div>
-//       <button
-//         className="p-1 bg-blue-700 text-white my-5"
-//         onClick={this.handleClick}
-//       >
-//         Toggle Image
-//       </button>
-//       {this.state.isShowing ? (
-//         <img
-//           src="https://images.unsplash.com/photo-1523215108660-3fdf7932d7a5?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1490&q=80"
-//           alt=""
-//         />
-//       ) : null}
-//     </div>
-//   </div>
-// </section>
-//     );
-//   }
-// }
 export default App;
--- likely-to-predict/src/components/Image.js
+++ likely-to-predict/src/components/Image.js
@@ -1,39 +1,68 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const Image = (props) => {
 
-    const Images = ["https://images.unsplash.com/photo-1612288528103-edc64749d4ad?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+    const [Images, setImages] = useState(["https://images.unsplash.com/photo-1612288528103-edc64749d4ad?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
                     "https://images.unsplash.com/photo-1598333286614-13d8b1767be0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
                     "https://images.unsplash.com/photo-1598185092528-a88552ff8040?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
                     "https://images.unsplash.com/photo-1553460982-e4d8b24bfdd9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80"
-                ];
+                ])
+    const [newImageURL, setNewImageURL] = useState("")
 
+
+  let count = 1;
   function ShowImage(){
     return (
+        
         Images.map((image) => {
           return (
-            <div>
-              <img key={image} src={image} width="200" height="150" alt="" />
+            <div className = "w-1/3 my-4">
+              <img key={count++} src={image} width="200" height="150" alt="" />
             </div>
           );
         })
       );
   }
 
-  const handleAdd = ()=>{
-      console.log("Working")
-      Images.push(
-        "https://images.unsplash.com/photo-1599239232659-097520265368?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-      );
+  const handleAdd = () => {
+      if(newImageURL != ""){
+          setImages([...Images, newImageURL]);    
+          setNewImageURL("")
+      }
   }
-  return <>
-    <div className ="flex justify-center">
-      <ShowImage/>
-    </div>
-    <div className = "flex justify-around my-5">
-        <input type="text"
-        className = "p-2 border border-gray-800 shadow rounded"/>
-        <button className = "p-2 bg-green-600 text-white" onClick = {handleAdd}>Add new</button>
-    </div>
-  </>
+
+  const handleChange = (event)=>{   
+      setNewImageURL(event.target.value);
+
+  }
+
+  return (
+    <>
+      <div className="flex flex-wrap justify-between ">
+        <ShowImage />
+      </div>
+
+      <div className="flex justify-between my-5 w-full">
+        <div className="w-full">
+          <input
+            type="text"
+            onChange={handleChange}
+            value={newImageURL}
+            className="p-2 border border-gray-800 shadow rounded w-full"
+          />
+        </div>
+        <div className="w-full">
+          <button
+            disabled={newImageURL === ""}
+            className={`p-2 text-white ml-2 ${
+              newImageURL !== "" ? "bg-green-600" : "bg-green-300"
+            }`}
+            onClick={handleAdd}
+          >
+            Add new
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
