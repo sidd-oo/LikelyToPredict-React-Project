88e4d2d8e7174a8ba885e331d4eb9ccb1e74f299
SIDDHARTH SAHOO <sahoosiddharth3@gmail.com>
2021-02-04 23:42:24 +0530

    Showing multiple images using Array.map()

--- likely-to-predict/src/components/Image.js
+++ likely-to-predict/src/components/Image.js
@@ -1,26 +1,27 @@
 import React from "react";
 
 export const Image = (props) => {
-//   const [myInterval, setmyInterval] = React.useState(null);
 
+    const Images = ["https://images.unsplash.com/photo-1612288528103-edc64749d4ad?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+                    "https://images.unsplash.com/photo-1598333286614-13d8b1767be0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+                    "https://images.unsplash.com/photo-1598185092528-a88552ff8040?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+                    "https://images.unsplash.com/photo-1553460982-e4d8b24bfdd9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80"
+                ];
 
-
-//Example of unmount, when the component is removed
-  React.useEffect(() => {
-    console.log("Image Mounted");
-  
-      const interval = setInterval(() => {
-        console.log("Hello");
-      },1000);
-    return () => {
-      console.log("Image Unmounted");
-      clearInterval(interval);
-    };
-    
-  }, []);
+  function ShowImage(){
+    return (
+        Images.map((image) => {
+          return (
+            <div>
+              <img key={image} src={image} width="200" height="150" alt="" />
+            </div>
+          );
+        })
+      );
+  }
   return (
-    <div>
-      <img src={props.src} alt="" />
+    <div className ="flex justify-center">
+      <ShowImage/>
     </div>
   );
 };
