011cebc0f34f43500e3647986e41ac42ea3e8b4c
SIDDHARTH SAHOO <sahoosiddharth3@gmail.com>
2021-02-07 20:40:48 +0530

    Using Unsplash API to fetch images

--- likely-to-predict/src/components/Images.js
+++ likely-to-predict/src/components/Images.js
@@ -1,29 +1,31 @@
 import React, { useState, useEffect, useRef} from "react";
-import ImageComp from './Image.js'
-
+import ImageComp from "./Image.js";
+import Axios from 'axios';
 export const Images = (props) => {
-  const [Image, setImages] = useState([
-    "https://images.unsplash.com/photo-1612288528103-edc64749d4ad?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-    "https://images.unsplash.com/photo-1598333286614-13d8b1767be0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-    "https://images.unsplash.com/photo-1598185092528-a88552ff8040?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-    "https://images.unsplash.com/photo-1553460982-e4d8b24bfdd9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80",
-  ]);
+  const [Image, setImages] = useState([]);
   const [newImageURL, setNewImageURL] = useState("");
 
   const inputRef = useRef(null);
 
   useEffect(() => {
     inputRef.current.focus();
-  },[])
+    Axios.get(
+      `${process.env.REACT_APP_UNSPLASH_URL}client_id=${process.env.REACT_APP_UNSPLASH_KEY}`
+    ).then((res) => {
+      setImages(res.data)
+      // console.log(res.data);
+    });
+  });
 
   function handleRemove(index) {
-    console.log("clicking");
     setImages(Image.filter((image, ind) => ind !== index));
+    console.log("I am changing state");
   }
-  
 
   function ShowImage() {
-    return Image.map((img, index) => <ImageComp image = {img} index = {index} handleRemove = {handleRemove}/>)
+    return Image.map((img, index) => (
+      <ImageComp image={img.urls.regular} index={index} handleRemove={handleRemove} />
+    ));
   }
 
   const handleAdd = () => {
@@ -38,35 +40,36 @@ export const Images = (props) => {
   };
 
   return (
-    <div className="justify-center">
-      <div className="flex flex-wrap justify-center ">
-        <ShowImage />
-      </div>
-
-      <div className="flex justify-center my-5 w-full">
-        <div className="w-full">
-          <input
-            id = "inputBox"
-            ref = {inputRef}
-            type="text"
-            onChange={handleChange}
-            value={newImageURL}
-            className="p-2 border border-gray-800 shadow rounded w-full"
-          />
+    <section>
+      <div className="justify-center">
+        <div className="flex flex-wrap justify-center ">
+          <ShowImage />
         </div>
 
-        <div className="">
-          <button
-            disabled={newImageURL === ""}
-            className={`p-2 w-40 rounded text-white ml-2 
-                  ${newImageURL !== "" ? "bg-green-600" : "bg-green-300"
-            }`}
-            onClick={handleAdd}
-          >
-            Add Image
-          </button>
+        <div className="flex justify-center my-5 w-full">
+          <div className="w-full">
+            <input
+              id="inputBox"
+              ref={inputRef}
+              type="text"
+              onChange={handleChange}
+              value={newImageURL}
+              className="p-2 border border-gray-800 shadow rounded w-full"
+            />
+          </div>
+
+          <div className="">
+            <button
+              disabled={newImageURL === ""}
+              className={`p-2 w-40 rounded text-white ml-2 
+            ${newImageURL !== "" ? "bg-green-600" : "bg-green-300"}`}
+              onClick={handleAdd}
+            >
+              Add Image
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
