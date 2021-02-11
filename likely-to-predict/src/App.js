74dccc57b66b17159ac7f9a66ff3a178396d7582
sidd-oo <sahoosiddharth3@gmail.com>
2021-02-11 21:41:51 +0530

    Nav bar component - Header

--- likely-to-predict/src/App.js
+++ likely-to-predict/src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./assets/css/tailwind.css";
+import Header from "./components/Header";
 import routes from "./utils/routes";
-
-
 
 let App = (props) => {
   return (
     <>
       <Router>
+        <Header/>
         <Switch>
-          { routes.map((route)=>(
-              <Route path = {route.path} exact = {route.exact} component = {route.component}/>
-            ))}
+          {routes.map((route) => (
+            <Route
+              path={route.path}
+              exact={route.exact}
+              component={route.component}
+            />
+          ))}
         </Switch>
       </Router>
     </>
